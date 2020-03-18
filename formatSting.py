296907b61892f7d26a9beed4f5e4a15bb9063fc8
Diego Gonçalves <ferreira.dfg@gmail.com>
2020-03-18 14:12:01 -0300

    format

---
+++ formatSting.py
@@ -0,0 +1,14 @@
+# o primeiro significa o tamanho total de caracteres a reservar, segundo numero de casa decimais 
+
+def age(idade):
+    print(" A idade é %03d" %idade)
+
+
+age(3)
+
+
+def salary(salary):
+    print("O salario é [%10.3f]" %salary)
+
+
+salary(5)
