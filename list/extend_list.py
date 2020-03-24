596e183d53e4f732e591bbfec2e959a255c6107d
Diego Gonçalves <ferreira.dfg@gmail.com>
2020-03-24 09:36:30 -0300

    extend list

---
+++ list/extend_list.py
@@ -0,0 +1,11 @@
+def extendz():
+    l=[]
+    i = 0
+    while True:
+        numero = int(input(f"digite o {i+1} "))
+        if numero == 0:
+            break
+        l.append(numero)
+
+
+extendz()
