a800fb1f3f04b0f67fda8bf9d4e2ffd9ecb78578
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2020-06-26 12:47:01 -0300

    sequencial de compras

---
+++ chapterVariables/315_timesequence.py
@@ -0,0 +1,10 @@
+def divida():
+    divida = 0
+    digito = 1
+    while digito > 0:
+        compra = int(input('Digite o valor da compra: '))
+        divida += compra
+        digito = int(input('Se deseja sair e somar sua dívida digite 0 ou digite 1 para continuar somando compras: '))
+    print(f'Sua divida é:{divida} reais')
+
+divida()
