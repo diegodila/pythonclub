0341204710b1b235974c439b9c7a1903eeaca0e0
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2020-06-27 10:44:35 -0300

    input soma

---
+++ chapterVariables/371_input_soma.py
@@ -0,0 +1,8 @@
+#Faça um programa que peça dois numeros inteiros. Imprima a soma desses dois numeros na tela.
+def soma_int():
+    x = int(input("Digite um numero x: "))
+    y = int(input("Digite um numero y: "))
+    print(f'A soma dos numeros é: {x + y}')
+
+
+soma_int()
