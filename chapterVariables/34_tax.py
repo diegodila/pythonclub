f39d43e0dbeaec2c4c0e04e464523994006b8b5c
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2020-07-31 18:12:15 -0300

    new code

--- chapterVariables/34_tax.py
+++ chapterVariables/34_tax.py
@@ -8,3 +8,7 @@ tax(1200)
 #outra forma, usando lambda 
 tax_lambda = lambda x: print("Taxadoo!!!") if x>=1200 else print('Não foi taxado!')
 tax_lambda(1330)
+
+#lambda não imprime, temos que usar a function print
+tax_lambda1 = lambda x: 'Taxado' if x >= 1200 else 'Not tax'
+print(tax_lambda1(1222))
