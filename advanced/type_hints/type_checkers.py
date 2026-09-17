a3f051c832912d1179577b302807106b1fb8aa3a
Diego Ferreira Gonçalves <ferreira.dfg@gmail.com>
2026-09-17 20:29:30 -0300

    adding new formatter with ruff

--- advanced/type_hints/type_checkers.py
+++ advanced/type_hints/type_checkers.py
@@ -1,6 +1,6 @@
-nome : int = "diego"
+nome: int = "diego"
 nome2 = "Diego"
-#command line |
-#uv tool install pyright
-#pyright type_checkers.py
-#uvx pyright type_checkers.py
+# command line |
+# uv tool install pyright
+# pyright type_checkers.py
+# uvx pyright type_checkers.py
